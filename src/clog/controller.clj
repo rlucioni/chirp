511645af0f4a185c7614803ff7c8fb15094d7261
Renzo Lucioni <renzolucioni@gmail.com>
2012-06-08 15:34:03 -0700

    added post listing and modified controller as necessary

--- src/clog/controller.clj
+++ src/clog/controller.clj
@@ -2,10 +2,15 @@
 (ns clog.controller
 	(:use clog.templates
 		  clog.models
-		  ring.util.response))
+		  ring.util.response
+		  korma.core))
 
 ;;; handler for the index page
 (defn index
 	"Index page handler"
 	[req]
-	(->> (home-page) response)) ;; sexy way of writing (reponse (home-page))
+	;;; select all posts using Korma's select function, and then pass them on to
+	;;; the homepage function; the result of the home-page template function - 
+	;;; the HTML with posts populated - is passed to the Ring's response function
+
+	(->> (select posts) home-page response)) ;;; sexy way of writing (reponse (home-page (select posts)))
--- src/clog/templates.clj
+++ src/clog/templates.clj
@@ -3,7 +3,12 @@
 
 ;;; using the deftemplate macro to create a template function called home-page
 ;;; which takes the file home.html and transforms the file using the forms specified
-(deftemplate home-page "home.html" []
+(deftemplate home-page "home.html" [posts]
 	;;; form in use - find the tag :title in home.html and replace its contents with
 	;;; the specified string
-	[:title] (content "Clog - The Clojure Blogging Engine"))
+	[:title] (content "Clog - The Clojure Blogging Engine")
+	;;; "clone" the div with class post and replace content of divs with classes title
+	;;; and content
+	[:div.post] (clone-for [post posts]
+				[:div.title] (content (:title post))
+				[:div.content (content (:content post))]))
