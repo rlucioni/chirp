0c7bdbfbcedb31fbac23cf9ccd0d4fd53c171248
Renzo Lucioni <renzolucioni@gmail.com>
2012-06-15 12:33:28 -0700

    can't place enlive helper function inside of str - trying external definition

--- src/chirp/templates.clj
+++ src/chirp/templates.clj
@@ -26,8 +26,8 @@
 	;;; the specified string
 
 	;;; NOTE: currently puts parens and quotes around the name in the title
-
-	[:title] (content (str "Chirp - " (content msg) "'s Posts"))
+	(def username (content msg))
+	[:title] (content (str "Chirp - " username "'s Posts"))
 	;;; show if the current user is logged in or not, and if so, as who
 	[:b.username] (content msg)
 	;;; "clone" the div with class post and replace content of divs with classes title
