9c9e70ad68b44948bce6903fdbc48cd9e42e0889
Jeff Dik <jeffdik@finecode.net>
2012-06-27 02:50:30 -0400

    set the database connection configuration in one spot
    that both Lobos and Korma reference

--- src/chirp/models.clj
+++ src/chirp/models.clj
@@ -2,15 +2,10 @@
 (ns chirp.models
   (:use [korma.db :only (defdb)]
         [korma.core :only (defentity)])
-  (:require [clojure.java.jdbc :as sql]))
+  (:require [chirp.db.config :as config]
+            [clojure.java.jdbc :as sql]))
 
-
-(defdb chirpdb
-  {:classname   "com.mysql.jdbc.Driver"
-   :subprotocol "mysql"
-   :subname     "//chirpdbinstance.cvnjt4wjg36t.us-east-1.rds.amazonaws.com:3306/chirpdb"
-   :user        "chirpuser"
-   :password    "lono1234"})
+(defdb chirpdb config/connection)
 
 (defentity authors)
 (defentity posts)
